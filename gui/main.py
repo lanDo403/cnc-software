a1004c24a51696eeef7aa2ed6914a0b7da4d836c
BIG-Denis <lozejor12@gmail.com>
2025-12-17 00:50:09 +0300

    [advanced_ui] Added structure and placeholder function for Ivan

--- gui/main.py
+++ gui/main.py
@@ -4,7 +4,22 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QTextCursor
 import sys
+from dataclasses import dataclass
 #import serial.tools.list_ports as get_list
+
+
+@dataclass
+class GCodePayload:
+    opcode: int  # one of: 0, 1, 2, 3, 90, 91
+    x: int
+    y: int
+
+
+def process_gcode(payload: GCodePayload) -> None:
+    print(f"GCode aqcquired: opcode={payload.code}, x={payload.x}, y={payload.y}")
+    # dummy placeholder
+    # Ivan, implement your logic here
+    pass
 
 
 class MainWindow(QMainWindow):
