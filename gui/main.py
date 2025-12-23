05f65a0294f763412dc7e1235f71b49c6c579e11
BIG-Denis <lozejor12@gmail.com>
2025-12-24 01:03:06 +0300

    [advanced_ui] Works everything except for connection check and drawings of hatch and arc

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QGraphicsScene
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QGraphicsScene, QFileDialog
 from PyQt5.QtSerialPort import QSerialPortInfo
 from PyQt5 import uic
 from PyQt5.QtCore import Qt, QObject, QEvent
@@ -11,9 +11,13 @@ import math
 #import serial.tools.list_ports as get_list
 
 # from response import *
-# dummy implementation of SendGcode for testing GUI
+# dummy implementation of SendGcode and SendHex for testing GUI
 def SendGcode(port: str, gcodeLines: list[str], baudrate: int, chunkSize: int=250, retries: int=3) -> bool:
     print(f"Sending G-codes: port={port}, baudrate={baudrate}; G-codes={gcodeLines}")
+    return True
+
+def SendHex(port: str, hexString: str, baudrate: int) -> bool:
+    print(f"Sending HEX packet: port={port}, baudrate={baudrate}; packet={hexString}")
     return True
 
 
@@ -61,14 +65,19 @@ class MainWindow(QMainWindow):
         self.paint_view.setScene(self.scene)
         self.paint_view.fitInView(self.scene.itemsBoundingRect(), Qt.KeepAspectRatio)
         self.mode_paint_label.setText(f'Горизонтальное перемещение')
+        self.file_gcodes: str = ''
 
-        # !TODO: button clock calls
+        self.btn_refresh.clicked.connect(self.GetCOMPorts)
         self.btn_dump_codes.clicked.connect(self.clicked_btn_dump_codes)
         self.btn_paint_calibrate.clicked.connect(self.clicked_btn_calibrate)
         self.btn_paint_clear_img.clicked.connect(self.clicked_btn_clear_img)
         self.btn_paint_vertical.clicked.connect(self.clicked_btn_mode_vrt)
         self.btn_paint_horizontal.clicked.connect(self.clicked_btn_mode_hrz)
         self.btn_paint_sloped.clicked.connect(self.clicked_btn_mode_slp)
+        self.btn_pro_gcode_send.clicked.connect(self.clicked_btn_send_gcode)
+        self.btn_pro_hex_send.clicked.connect(self.clicked_btn_send_hex)
+        self.btn_select_file.clicked.connect(self.clicked_btn_select_file)
+        self.btn_send_file.clicked.connect(self.clicked_btn_send_file)
         # TBD
 
         self.mode: str = 'hrz'  # hrz, vrt, slp, htc, arc
@@ -90,10 +99,18 @@ class MainWindow(QMainWindow):
         if pre is not None:
             local_commands.append(pre)
         # all commands
-        for command in local_commands:
+        while len(local_commands) > 0:
+            command = local_commands.pop(0)
+            if len(local_commands) != 0 or pre is None:
+                color = 'black'
+            else:
+                if self.btn_radio_paint.isChecked():
+                    color = 'green'
+                else:
+                    color = 'orange'
             if command[0] in ('hrz', 'vrt', 'slp'):
                 x1, y1, x2, y2 = command[1:]
-                draw.line((x1, y1, x2, y2), fill="black" if command != pre else 'green', width=2)
+                draw.line((x1, y1, x2, y2), fill=color, width=2)
             pass
         # current position
         if self.current_x is not None and self.current_y is not None:
@@ -108,14 +125,14 @@ class MainWindow(QMainWindow):
         ports = list(self.getPorts.availablePorts())
         self.portsComboBox.clear()
         if ports:
-            self.Append('<<<<ПОРТЫ НАЙДЕНЫ>>>>\n')
-            self.Append('Список портов:\n')
+            self.Append('<<<< ПОРТЫ НАЙДЕНЫ >>>>\n')
+            self.Append(' Список портов: \n')
             for port in ports:
                 self.portsComboBox.addItem(port.portName())
                 self.Append(port.portName())
-            self.Append('\n<<<<ВЫПАДАЮЩИЙ СПИСОК ЗАПОЛНЕН ВСЕМИ ДОСТУПНЫМИ ПОРТАМИ>>>>\n')
+            self.Append('\n<<<< ВЫПАДАЮЩИЙ СПИСОК ЗАПОЛНЕН ВСЕМИ ДОСТУПНЫМИ ПОРТАМИ >>>>\n')
         else:
-            self.Append('<<<<ПОРТЫ НЕ ОБНАРУЖЕНЫ>>>>\n')
+            self.Append('<<<< ПОРТЫ НЕ ОБНАРУЖЕНЫ >>>>\n')
 
     def Append(self, text):
         cursor = self.consoleEdit.textCursor()
@@ -129,11 +146,17 @@ class MainWindow(QMainWindow):
         goto_y = y
         if self.mode == 'hrz':
             goto_y = self.current_y
+            if self.btn_radio_snapping.isChecked():
+                nearest_x, nearest_y = nearest_anchor(x, y, self.anchors)
+                goto_x = nearest_x
         if self.mode == 'vrt':
             goto_x = self.current_x
+            if self.btn_radio_snapping.isChecked():
+                nearest_x, nearest_y = nearest_anchor(x, y, self.anchors)
+                goto_y = nearest_y
         if self.mode == 'slp' and self.btn_radio_snapping.isChecked():
             goto_x, goto_y = nearest_anchor(goto_x, goto_y, self.anchors)
-        print(f"Will go to x={goto_x}, y={goto_y}")
+        self.Append(f"<<<< Движение на x={goto_x}, y={goto_y} >>>>\n")
         # pre-paint and confirm
         self.draw_img(pre=(self.mode, self.current_x, self.current_y, goto_x, goto_y))  # pre-paint
         if not confirm(self, f"Вы уверены, что хотите выполнить это действие?"):
@@ -162,6 +185,7 @@ class MainWindow(QMainWindow):
                 return
         with open('g_codes_dump.cnc', 'w') as file:
             file.write('\n'.join(self.g_codes))
+        self.Append('<<<< G-коды сдамплены в файл "g_codes_dump.cnc" >>>>\n')
         alert(self, 'G-коды сдаплены в файл "g_codes_dump.cnc" успешно!')
 
     def clicked_btn_calibrate(self):
@@ -172,6 +196,7 @@ class MainWindow(QMainWindow):
         if not SendGcode(self.portsComboBox.currentText(), ["G00 X0 Y0",], int(self.baudRateLineEdit.text())):
             alert(self, "Ошибка отправки! Проверьте подключение!")
             return
+        self.Append("<<<< ЧПУ отклабирован >>>>\n")
         self.g_codes.append('G00 X0 Y0')
         self.draw_img()
 
@@ -181,6 +206,7 @@ class MainWindow(QMainWindow):
         self.draw_commands = []
         self.g_codes = []
         print("Image cleared")
+        self.Append("<<<< Изображение очищено >>>>\n")
         self.draw_img()
 
     def clicked_btn_mode_hrz(self):
@@ -194,6 +220,44 @@ class MainWindow(QMainWindow):
     def clicked_btn_mode_slp(self):
         self.mode = 'slp'
         self.mode_paint_label.setText(f'Произвольное перемещение')
+
+    def clicked_btn_select_file(self):
+        self.file_gcodes, _ = QFileDialog.getOpenFileName(self, "Выберите файл", "", "Все файлы (*.*)")
+        self.label_filename.setText(f"Выбранный файл: {self.file_gcodes}")
+
+    def clicked_btn_send_file(self):
+        if not confirm(self, f"Вы уверены, что хотите отправить файл {self.file_gcodes}?"):
+            return
+        self.file_gcodes_content = []
+        with open(self.file_gcodes, 'r') as file:
+            file_gcodes_content = file.readlines()
+            for line in file_gcodes_content:
+                line: str = line.replace('\n', '')
+                if line != '':
+                    self.file_gcodes_content.append(line)
+        if not SendGcode(self.portsComboBox.currentText(), self.file_gcodes_content, int(self.baudRateLineEdit.text())):
+            alert(self, "Ошибка отправки! Проверьте подключение!")
+            return
+        self.Append(f'<<<< Успешно отправлен файл с G-кодами: {self.file_gcodes} >>>>\n')
+        alert(self, "Отправлено успешно!")
+
+    def clicked_btn_send_gcode(self):
+        if not confirm(self, "Вы уверены, что хотите отправить этот G-код?"):
+            return
+        if not SendGcode(self.portsComboBox.currentText(), [self.lineEdit_pro_gcode.text(),], int(self.baudRateLineEdit.text())):
+            alert(self, "Ошибка отправки! Проверьте подключение!")
+            return
+        self.Append(f'<<<< Успешно отправлен G-код: {self.lineEdit_pro_gcode.text()} >>>>\n')
+        alert(self, "Отправлено успешно!")
+
+    def clicked_btn_send_hex(self):
+        if not confirm(self, "Вы уверены, что хотите отправить этот HEX пакет?"):
+            return
+        if not SendHex(self.portsComboBox.currentText(), self.lineEdit_pro_hex.text(), int(self.baudRateLineEdit.text())):
+            alert(self, "Ошибка отправки! Проверьте подключение!")
+            return
+        self.Append(f'<<<< Успешно отправлен пакет: {self.lineEdit_pro_hex.text()} >>>>\n')
+        alert(self, "Отправлено успешно!")
 
 
 if __name__ == "__main__":
